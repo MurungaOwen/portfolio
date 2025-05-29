ef79a77fdc831898eb027447cb8928298c2a253d
MurungaOwen <owenhood80@gmail.com>
2025-05-29 14:29:27 +0300

    correct base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '', //REPO name for deploying on github pages
+  base: '/portfolio/', //REPO name for deploying on github pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
