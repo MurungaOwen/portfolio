ca53b49ec92e6d3e64b29bf935ee7d288d9b5b15
MurungaOwen <owenhood80@gmail.com>
2025-06-04 22:08:32 +0300

    feat: add links data for GitHub, LinkedIn, email, and CVn  in one place for easy mod

---
+++ src/data/links.ts
@@ -0,0 +1,24 @@
+// create links for github, twitter, cv, email, linkedin
+const links = {
+  github: {
+    url: 'https://github.com/MurungaOwen',
+    icon: 'https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/github.svg',
+    color: '#181717',
+  },
+  linkedin: {
+    url: 'https://www.linkedin.com/in/owen-murunga/',
+    icon: 'https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/linkedin.svg',
+    color: '#0077B5',
+  },
+  email: {
+    url: 'mailto:owenhood80@gmail.com',
+    icon: 'https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/gmail.svg',
+    color: '#EA4335',
+  },
+  cv: {
+    url: 'https://docs.google.com/document/d/1yFw7lQ2qWKCWZzKTCDQPSBY2PZPrYY3HS4-Z5IUZtTw/export?format=pdf',
+    icon: 'https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/file-pdf.svg',
+    color: '#E63946',
+  }
+};
+export default links;
